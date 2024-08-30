df6655344c478d83fc6a8d145f4a2d5e7ab97ede
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-08-30 22:55:58 +0700

    first commit

---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+import axios from "../axios";
+
+const handleLoginApi = (userEmail, userPassword) => {
+    return axios.post('/api/login', {email: userEmail, password: userPassword});
+}
+
+const getAllUsers = (inputId) => {
+    //template string
+    return axios.get(`/api/get-all-user?id=${inputId}`)
+}
+
+export { handleLoginApi, getAllUsers }
+
