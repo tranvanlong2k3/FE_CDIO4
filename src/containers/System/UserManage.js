bfb36f1b54993a15a6ced29cd025e1696ad55f39
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-05 00:12:25 +0700

    Third commit

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,15 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import {getAllUsers} from '../../services/userService';
+import { bind } from 'lodash';
+import ModalUser from './ModalUser';
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -21,12 +24,35 @@ class UserManage extends Component {
         }
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
 
     render() {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser 
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFormParent={this.toggleUserModal}
+                    test={'abc'}
+
+                />
                 <div className='title text-center'>Manage users with Lio</div>
+                <div className='mx-1'>
+                    <button 
+                    className='btn btn-primary px-3'
+                    onClick={()=>this.handleAddNewUser()}
+                    ><i className="fas fa-plus"></i> Add new users</button>
+                </div>
                 <div className='users-table mt-3 mx-1'>
                     <table id="customers">
                         <tr>
@@ -38,7 +64,6 @@ class UserManage extends Component {
                         </tr>
                         
                             {arrUsers && arrUsers.map((item, index) => {
-                                console.log('Lio check map', item, index)
                                     return (
                                         <tr key={index}>
                                             <td>{item.email}</td>
