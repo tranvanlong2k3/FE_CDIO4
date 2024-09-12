c7310a6f85330031bef3cc3c8313e5dc0c040c63
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-13 00:41:08 +0700

    Thirteenth commit

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
-import Specialty from './Section/Specialty'
+import Specialty from './Section/Specialty';
+import Medical from "./Section/Medical";
+import OutStandingDoctor from './Section/OutStandingDoctor';
+import HandBook from './Section/HandBook';
+import './HomePage.scss';
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        };
 
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
-                <div style={{height: '300px'}}></div>
+                <Specialty 
+                    settings = {settings} 
+                />
+                <Medical 
+                    settings = {settings} 
+                />
+                <OutStandingDoctor 
+                    settings = {settings}
+                />
+                <HandBook 
+                    settings = {settings}
+                />
             </div>
         );
     }
