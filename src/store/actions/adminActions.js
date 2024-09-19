def95d2413182151b245b12ad6a7d9511679acdd
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-20 00:34:44 +0700

    Twentieth commit

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -8,6 +8,8 @@ import { getAllCodeService } from '../../services/userService';
 export const fetchGenderStart = () => {
     return async(dispatch, getState) => {
         try {
+            dispatch({ type: actionTypes.FETCH_GENDER_START })
+
             let res = await getAllCodeService("GENDER");
             if(res && res.errCode === 0) {
                 dispatch(fetchGenderSuccess(res.data));
@@ -29,3 +31,53 @@ export const fetchGenderSuccess = (genderData) => ({
 export const fetchGenderFaided = () => ({
     type: actionTypes.FETCH_GENDER_FAIDED
 })
+
+export const fetchPositionSuccess = (positionData) => ({
+    type: actionTypes.FETCH_POSITION_SUCCESS,
+    data: positionData
+})
+
+export const fetchPositionFaided = () => ({
+    type: actionTypes.FETCH_POSITION_FAIDED
+})
+
+export const fetchRoleSuccess = (roleData) => ({
+    type: actionTypes.FETCH_ROLE_SUCCESS,
+    data: roleData
+})
+
+export const fetchRoleFaided = () => ({
+    type: actionTypes.FETCH_ROLE_FAIDED
+})
+
+export const fetchPositionStart = () => {
+    return async(dispatch, getState) => {
+        try {
+            let res = await getAllCodeService("POSITION");
+            if(res && res.errCode === 0) {
+                dispatch(fetchPositionSuccess(res.data));
+            } else {
+                dispatch(fetchPositionFaided());
+            }
+        } catch (e) {
+            dispatch(fetchPositionFaided());
+            console.log('fetchPositionStart error', e)
+        }
+    }
+}
+
+export const fetchRoleStart = () => {
+    return async(dispatch, getState) => {
+        try {
+            let res = await getAllCodeService("ROLE");
+            if(res && res.errCode === 0) {
+                dispatch(fetchRoleSuccess(res.data));
+            } else {
+                dispatch(fetchRoleFaided());
+            }
+        } catch (e) {
+            dispatch(fetchRoleFaided());
+            console.log('fetchRoleStart error', e)
+        }
+    }
+}
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,6 +1,7 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
+    isLoadingGender: false,
     gender: [],
     roles: [],
     position: []
@@ -9,16 +10,42 @@ const initialState = {
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_GENDER_START:
-            return {
-                ...state
-            }
-        case actionTypes.FETCH_GENDER_SUCCESS:
-            let copyState = {...state};
-            copyState.genders = action.data;
+            let copyState = { ...state };
+            copyState.isLoadingGender = true;
             return {
                 ...copyState
             }
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data;
+            state.isLoadingGender = false;
+            return {
+                ...state
+            }
         case actionTypes.FETCH_GENDER_FAIDED:
+            state.isLoadingGender = false;
+            state.genders = [];
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_POSITION_FAIDED:
+            state.positions = [];
+            return {
+                ...state
+            }
+
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_ROLE_FAIDED:
+            state.roles = [];
             return {
                 ...state
             }
