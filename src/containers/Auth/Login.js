da7ef0077f7fa294f1fccbc1e6ad476ca22450fc
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-28 19:18:36 +0700

    Thirty-firsth commit

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -63,6 +63,12 @@ class Login extends Component {
         })
     }
 
+    handleKeyDown = (event) => {
+        if(event.key === 'Enter' || event.keyCode === 13) {
+            this.handleLogin();
+        }
+    }
+
     render() {
         return (
             <div className='login-background'>
@@ -76,6 +82,7 @@ class Login extends Component {
                                 placeholder='Enter your username'
                                 value={this.state.username}
                                 onChange={(event) => this.handleOnChangeUsername(event)}
+                                onKeyDown={(event) => this.handleKeyDown(event)}
                             />
                         </div>
                         <div className='col-12 form-group login-input'>
