51b25d20591468df4b883b3bbcd442e2f2545a83
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-30 01:35:47 +0700

    Thirty-fourth commit

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,7 +4,7 @@ import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss';
 import { getDetailInforDoctor } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
-
+import DoctorSchedule from './DoctorSchedule';
 class DetailDoctor extends Component {
 
     constructor(props) {
@@ -67,13 +67,20 @@ class DetailDoctor extends Component {
                         </div>
                     </div>
                     <div className='schedule-doctor'>
+                        <div className='content-left'>
+                                <DoctorSchedule 
+                                    doctorIdFromParent={detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                                />
+                        </div>
+                        <div className='content-right'>
 
+                        </div>
                     </div>
                     <div className='detail-infor-doctor'>
                         {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
                             &&
-                            <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML }}>
-                                
+                            <div dangerouslySetInnerHTML={{ __html: detailDoctor.Markdown.contentHTML }}>
+
                             </div>
                         }
                     </div>
